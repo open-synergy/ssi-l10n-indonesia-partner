d392b4e5f3f356a6faf06f34e49633feec704e4a
mikevhe18 <viriyananda.michael@gmail.com>
2023-06-21 16:34:44 +0700

    [ssi_l10n_id_partner_identification_kependudukan] 14.0.1.0.0

---
+++ ssi_l10n_id_partner_identification_kependudukan/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Partner Identification - Identitas Kependudukan",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_partner_identification",
+    ],
+    "data": [
+        "data/res_partner_id_category_data.xml",
+    ],
+    "demo": [],
+}
